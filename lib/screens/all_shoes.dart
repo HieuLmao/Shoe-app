0def025a35cfd1bd39359eb29e1f956ae9c645eb
hieu nguyen <hieumi0772@gmail.com>
2023-06-01 02:45:13 +0700

    all shoes

---
+++ lib/screens/all_shoes.dart
@@ -0,0 +1,52 @@
+import 'package:flutter/material.dart';
+import 'package:ionicons/ionicons.dart';
+import 'package:provider/provider.dart';
+import 'package:shoe_app/styles/styles.dart';
+import 'package:shoe_app/widgets/appbar.dart';
+import 'package:shoe_app/widgets/export.dart';
+
+import '../provider/shoe_provider.dart';
+
+class AllShoes extends StatelessWidget {
+  static const routesName = 'all-shoes';
+  const AllShoes({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    final response = Provider.of<ShoeProvider>(context, listen: false).shoes;
+
+    return AppBarWidget(
+        title: 'All Shoes',
+        leadingIcon: GestureDetector(
+          onTap: () {
+            Navigator.of(context).pop();
+          },
+          child: Icon(
+            Ionicons.arrow_back,
+            color: black,
+          ),
+        ),
+        trailingIcon: Icon(
+          Ionicons.menu,
+          color: black,
+        ),
+        child: Column(
+          children: [
+            const SearchWidget(),
+            Expanded(
+              child: GridView.builder(
+                gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+                    crossAxisCount: 2,
+                    childAspectRatio: 1,
+                    mainAxisSpacing: small,
+                    crossAxisSpacing: small),
+                itemBuilder: (context, index) => ChangeNotifierProvider.value(
+                  value: response[index],
+                  child: const ShoeItem(),
+                ),
+              ),
+            )
+          ],
+        ));
+  }
+}
